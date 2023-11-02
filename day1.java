ffbea549cf528a56aaf742371e3e3cc3d0f1c819
clara j <124330368+pinkbagel@users.noreply.github.com>
2023-11-02 22:43:48 +0000

    yuh

---
+++ day1.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.util.*;
+
+public class day1{
+    public static void main(String[] args){
+        File myObj = new File("input.txt");
+        Scanner myReader = new Scanner(myObj);
+    }
+    public calorieCount(){
+        String elves = 
+    }
+
+}
---
+++ day1.py
@@ -0,0 +1,2 @@
+with open("input.txt") as file:
+    data = [i for i in ]
